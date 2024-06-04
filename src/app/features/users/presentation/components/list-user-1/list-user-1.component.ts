1ac9210a6469b44b49b71089acf7f3128ec00ae2
Miguel Sanz <msanz1610@gmail.com>
2024-06-04 12:10:42 -0400

    feat: Agregar lazy loading para imágenes de perfil de usuario

--- src/app/features/users/presentation/components/list-user-1/list-user-1.component.ts
+++ src/app/features/users/presentation/components/list-user-1/list-user-1.component.ts
@@ -1,17 +1,17 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslocoModule } from '@ngneat/transloco';
 import { IUser } from '@users';
+import { commonModules } from '@shared';
 
 @Component({
   selector: 'list-user-1',
   templateUrl: 'list-user-1.component.html',
   styleUrl: './list-user.component.scss',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule, TranslocoModule],
+  imports: [...commonModules, MatButtonModule, MatIconModule, RouterModule, TranslocoModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListUser1Component {
