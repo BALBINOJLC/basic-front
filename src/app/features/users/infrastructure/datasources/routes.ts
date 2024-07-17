6a53ddb62f0477ca32b58db42214694dd2dda50f
Miguel Sanz <msanz1610@gmail.com>
2024-07-17 16:44:32 -0400

    feat(user): update registration route to use "signup" instead of "register"

--- src/app/features/users/infrastructure/datasources/routes.ts
+++ src/app/features/users/infrastructure/datasources/routes.ts
@@ -1,7 +1,7 @@
 export const apiRestRoutes = {
   gets: 'users',
   get: 'users/get-user',
-  add: 'auth/register',
+  add: 'auth/signup',
   put: 'users',
   delete: 'users',
   search: 'users/search-users',
