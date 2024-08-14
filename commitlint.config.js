c8471f7e6ca3e81dc1dcfedb94a680b825bf7806
Miguel Sanz <msanz1610@gmail.com>
2024-08-14 18:08:09 -0400

    feat(commitlint): update commitlint.config.js with new rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,6 +9,7 @@ module.exports = {
     'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 1000]
+    'header-max-length': [2, 'always', 200],
+    'body-max-line-length': [2, 'always', 1000],
   }
 };
